28e90e5629eb635f1a07682d27281bfdda161ea5
Nicolas <ni.silvaf@duocuc.cl>
2026-05-16 14:32:17 -0400

    Agregado CORS para frontend React

--- students-service/src/main/java/cl/duoc/bernardo_ohiggins/students_service/controller/EstudianteController.java
+++ students-service/src/main/java/cl/duoc/bernardo_ohiggins/students_service/controller/EstudianteController.java
@@ -12,6 +12,7 @@ import reactor.core.scheduler.Schedulers;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class EstudianteController {
 
     private final EstudianteService estudianteService;
