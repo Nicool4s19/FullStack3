d44a9df56a78699b50bd22fd245b0173f18daac0
MKbenjA9 <xbenjamin629@gmail.com>
2026-05-17 14:53:56 -0400

    bb

--- src/main/java/com/infraestructura/academica/repositories/CursoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infraestructura.academica.repositories;
-
-import com.infraestructura.academica.models.entities.Curso;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CursoRepository extends JpaRepository<Curso, Long> {
-}
