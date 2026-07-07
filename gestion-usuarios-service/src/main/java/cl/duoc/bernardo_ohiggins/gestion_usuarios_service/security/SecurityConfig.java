89a876d9192e40563a73f749db242532046ea3da
MKbenjA9 <xbenjamin629@gmail.com>
2026-07-07 14:12:57 -0400

    Configuración de SecurityConfig

--- gestion-usuarios-service/src/main/java/cl/duoc/bernardo_ohiggins/gestion_usuarios_service/security/SecurityConfig.java
+++ gestion-usuarios-service/src/main/java/cl/duoc/bernardo_ohiggins/gestion_usuarios_service/security/SecurityConfig.java
@@ -1,5 +1,25 @@
 package cl.duoc.bernardo_ohiggins.gestion_usuarios_service.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
-    
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/usuarios/login").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
 }
