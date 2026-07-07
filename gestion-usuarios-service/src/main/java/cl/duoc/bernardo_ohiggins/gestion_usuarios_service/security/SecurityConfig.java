4d17d39e219c0e746e32c1c49c2caf30b1e0b650
MKbenjA9 <xbenjamin629@gmail.com>
2026-07-07 17:38:11 -0400

    porfa ahora si

--- gestion-usuarios-service/src/main/java/cl/duoc/bernardo_ohiggins/gestion_usuarios_service/security/SecurityConfig.java
+++ gestion-usuarios-service/src/main/java/cl/duoc/bernardo_ohiggins/gestion_usuarios_service/security/SecurityConfig.java
@@ -2,24 +2,83 @@ package cl.duoc.bernardo_ohiggins.gestion_usuarios_service.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/usuarios/login").permitAll()
-                        .anyRequest().permitAll()
-                )
-                .httpBasic(Customizer.withDefaults());
+            .csrf(csrf -> csrf.disable())
+
+            .cors(cors -> {})
+
+            .authorizeHttpRequests(auth -> auth
+
+                .requestMatchers(
+                    "/api/usuarios/login",
+                    "/api/usuarios"
+                ).permitAll()
+
+                .anyRequest().permitAll()
+            );
+
 
         return http.build();
+    }
+
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(
+            List.of(
+                "https://full-stack3-front.vercel.app"
+            )
+        );
+
+
+        config.setAllowedMethods(
+            List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+            )
+        );
+
+
+        config.setAllowedHeaders(
+            List.of("*")
+        );
+
+
+        config.setAllowCredentials(true);
+
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+
+        source.registerCorsConfiguration(
+            "/**",
+            config
+        );
+
+
+        return source;
     }
 }
